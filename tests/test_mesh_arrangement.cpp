c9b7a8cc232db222c19e8badaf4836ead80d6898
Qingnan Zhou <qzhou@adobe.com>
2024-11-30 10:03:59 -0500

    Add unit tests.

---
+++ tests/test_mesh_arrangement.cpp
@@ -0,0 +1,9 @@
+#include <arrangement/FastArrangement.h>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+TEST_CASE("FastArrangement", "[fast_arrangement][arrangement]")
+{
+    arrangement::FastArrangement arrangement;
+}
