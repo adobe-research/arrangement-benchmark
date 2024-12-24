9238caccaafb09db73754a45672e854669722bd5
Qingnan Zhou <qzhou@adobe.com>
2024-12-24 15:07:59 -0500

    Update python unit test.

--- python/tests/test_arrangement.py
+++ python/tests/test_arrangement.py
@@ -3,6 +3,7 @@ import lagrange
 import arrangement
 import numpy as np
 
+
 @pytest.fixture
 def tet():
     mesh = lagrange.SurfaceMesh()
@@ -15,6 +16,7 @@ def tet():
     mesh.add_triangle(0, 1, 3)
     mesh.add_triangle(1, 2, 3)
     return mesh
+
 
 class TestArrangement:
     def compute_arrangement(self, mesh, engine_name):
@@ -37,7 +39,9 @@ class TestArrangement:
         output_mesh = lagrange.SurfaceMesh()
         output_mesh.add_vertices(engine.vertices)
         output_mesh.add_triangles(engine.faces)
-        output_mesh.create_attribute("label", initial_values=engine.face_labels)
+        output_mesh.create_attribute(
+            "label", element="Facet", initial_values=engine.face_labels
+        )
 
         cells = []
         for ci in range(engine.num_cells):
@@ -49,18 +53,35 @@ class TestArrangement:
 
         return output_mesh, cells
 
+    def test_mesh_arrangement(self, tet):
+        r, cells = self.compute_arrangement(tet, "mesh")
+        assert len(cells) == 2
+
+        tet2 = tet.clone()
+        tet2.vertices = tet.vertices + [0.2, 0.2, 0.2]
+
+        mesh = lagrange.combine_meshes([tet, tet2])
+        r, cells = self.compute_arrangement(mesh, "mesh")
+        assert len(cells) == 4
+
+    def test_fast_arrangement(self, tet):
+        r, cells = self.compute_arrangement(tet, "fast")
+        assert len(cells) == 2
+
+        tet2 = tet.clone()
+        tet2.vertices = tet.vertices + [0.2, 0.2, 0.2]
+
+        mesh = lagrange.combine_meshes([tet, tet2])
+        r, cells = self.compute_arrangement(mesh, "fast")
+        assert len(cells) == 4
 
     def test_geogram_arrangement(self, tet):
         r, cells = self.compute_arrangement(tet, "geogram")
-        lagrange.io.save_mesh("debug.obj", r)
+        assert len(cells) == 2
 
         tet2 = tet.clone()
-        tet2.vertices = tet.vertices + [10.2, 0.2, 0.2]
+        tet2.vertices = tet.vertices + [0.2, 0.2, 0.2]
 
         mesh = lagrange.combine_meshes([tet, tet2])
         r, cells = self.compute_arrangement(mesh, "geogram")
-        lagrange.io.save_mesh("debug2.obj", r)
-
-        for i, cell in enumerate(cells):
-            print(cell.facets)
-            lagrange.io.save_mesh(f"cell_{i}.msh", cell)
+        assert len(cells) == 4
